ccec13b16b092bfbc17e16f0ecf266c22d773f4e
industArk <achoruzy@gmail.com>
2021-03-29 21:41:55 +0200

    working 0054 rev.2

--- 0054_Poker_hands.py
+++ 0054_Poker_hands.py
@@ -52,10 +52,6 @@ def split_players(card_list: list) -> tuple:
     return cards_split
 
 
-def check_cards_ranking():
-    pass
-
-
 def compare_cards(plr1: int, pl2: int, *args) -> int:
     """ Compares card rankings of players. """
     rankings = [i for i in (plr1, pl2, *args)]
@@ -71,52 +67,73 @@ class Cards():
 
     def __init__(self, cards: list):
         self.cards = cards
-        self.cards_string = self.cards_str()
+        self.cards.sort()
+        self.cards_string = self._cards_str()
 
-    def cards_str(self):
+    def _cards_str(self):
         cards_string = ''
+
         for i in self.cards:
             cards_string += i
+
         return cards_string
 
-    def no_ranked_cards(self):
+    def _all_color(self) -> bool:
+        """ Checks if all cards are the same color."""
+        check_set = set()
+
+        for card in self.cards:
+            check_set.add(card[1])
+
+        if len(check_set) != 5:
+            return True
+        return False
+
+    def no_ranked_cards(self) -> bool:
+        """ Method checks if card set has any ranked figures. If has, returns True."""
         for i in self.values:
             if self.cards_string.count(i) > 1:
                 return False
         for i in self.colors:
             if self.cards_string.count(i) > 2:
                 return False
+
         return True
 
-    def highest_card(self):
-        highest_card = 0
-        for i in self.cards:
-            key = i[0]
-            if self.scoring[key] > highest_card:
-                highest_card = self.scoring[key]
+    def highest_card(self) -> dict:
+        """ Method checks the highest card in card set."""
+        highest_card = {}
+        highest_score = 0
+
+        for card in self.cards:
+            key = card[0]
+            if self.scoring[key] > highest_score:
+                highest_score = self.scoring[key]
+                highest_card = {key: self.scoring[key]}
+
         return highest_card
 
-    def has_pairs(self):
-        pass
+    def repeated_card_values(self) -> dict:
+        """ Method checks if pairs, threes and fours of cards of one kind occurs in a set."""
+        result = dict()
 
-    def has_three(self):
-        pass
+        for val in self.values:
+            count = self.cards_string.count(val)
+            if count > 1:
+                result.update({val: count})
 
-    def has_straight(self):
-        pass
+        return result
 
-    def has_flush(self):
-        pass
+    def straight_flush(self) -> dict:
+        """ Method checks Straight, Flush, Straight Flush and Royal Flush type figures for a card set."""
+        result = {}
 
-    def has_full_house(self):
-        pass
+        if self._all_color():
+            highest = self.highest_card()
+            val = list(highest.values())[0]
+            result = {'Flush': val}
 
-    def has_four(self):
-        pass
-
-    def has_flush(self):
-        pass
-
+        return result
 
 # -------------- TESTS ---------------
 
@@ -147,12 +164,29 @@ def test_split_players():
 def test_Cards():
     card_set_1 = ['8C', 'TS', 'KC', '9H', '4S']
     card_set_2 = ['8C', '8S', 'TS', 'JS', 'QS']
+    card_set_3 = ['8C', '8S', 'TS', 'TC', 'TD']
+    card_set_4 = ['QC', 'QS', 'QD', 'QH', 'AS']
+    card_set_5 = ['2S', '5S', '7S', 'TS', 'QS']
+    card_set_6 = ['8C', '8S', 'TS', 'TC', 'TD']
+    card_set_7 = ['QC', 'QS', 'QD', 'QH', 'AS']
+
     cards_1 = Cards(card_set_1)
     cards_2 = Cards(card_set_2)
+    cards_3 = Cards(card_set_3)
+    cards_4 = Cards(card_set_4)
+    cards_5 = Cards(card_set_5)
+    cards_6 = Cards(card_set_6)
+    cards_7 = Cards(card_set_7)
+
     # then
     assert cards_1.no_ranked_cards() == True
     assert cards_2.no_ranked_cards() == False
-    assert cards_1.highest_card() == 11
+    assert cards_1.highest_card() == {'K': 11}
+    assert cards_1.repeated_card_values() == {}
+    assert cards_2.repeated_card_values() == {'8': 2}
+    assert cards_3.repeated_card_values() == {'8': 2, 'T': 3}
+    assert cards_4.repeated_card_values() == {'Q': 4}
+    assert cards_5.straight_flush() == {'Flush': 10}
 
 
 def test_compare_cards():
@@ -168,9 +202,6 @@ def test_compare_cards():
 
 # --------------- RUN ---------------
 if __name__ == '__main__':
-    card_set = ['8C', 'TS', 'KC', '9H', '4S']
-    cards_1 = Cards(card_set)
-    print(cards_1.highest_card())
-
+    pass
 
 # ------------ RESULT -------------
